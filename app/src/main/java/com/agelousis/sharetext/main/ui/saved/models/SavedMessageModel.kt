a17aa3cb7ac6b202cef12bddc43eb15afdbba19e
Vagelis Agelousis <vagelis_agelousis@outlook.com>
2020-01-21 23:18:43 +0200

    Saved Text header design updates

--- app/src/main/java/com/agelousis/sharetext/main/ui/saved/models/SavedMessageModel.kt
+++ app/src/main/java/com/agelousis/sharetext/main/ui/saved/models/SavedMessageModel.kt
@@ -1,3 +1,14 @@
 package com.agelousis.sharetext.main.ui.saved.models
 
-data class SavedMessageModel(val ID: Long? = null, val channel: String, val text: String, val date: String, var showLine: Boolean = true, var colorOfCircleHeaderBackground: Int? = null, var colorOfCircleHeader: Int? = null)
+import java.lang.StringBuilder
+
+data class SavedMessageModel(val ID: Long? = null, val channel: String, val text: String, val date: String, var showLine: Boolean = true, var colorOfCircleHeaderBackground: Int? = null, var colorOfCircleHeader: Int? = null) {
+
+    val joinedChannelLetters: String
+        get() {
+            val builder = StringBuilder()
+            for (i in 0..1) { builder.append(this.channel.split(" ").getOrNull(i)?.firstOrNull()) }
+            return builder.toString()
+        }
+
+}
